00a04ad962db13a254082497d444ee09414e0453
Qi Liu <liuq901@163.com>
2016-04-18 14:41:40 +0800

    finish hard margin svm

--- hw3/code/hw3.py
+++ hw3/code/hw3.py
@@ -1,6 +1,8 @@
 import csv
 import re
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
 from sklearn import svm
 
 def get_data(file_name):
@@ -41,7 +43,7 @@ def get_array(file_name):
     label = get_field(data, label_name)
     return feature, label
 
-def main():
+def spam_filter():
     for kernel in ['linear', 'poly', 'rbf']:
         feature, label = get_array('train.csv')
         clf = svm.SVC(kernel = kernel)
@@ -58,5 +60,37 @@ def main():
         result_file.write('precision: ' + str(precision) + '\n')
         result_file.write('recall: ' + str(recall) + '\n')
         result_file.close()
+
+def plot(positive_x, positive_y, negative_x, negative_y, boundary, name):
+    plt.figure(figsize = (5, 5))
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.xlim(-3.5, 3.5)
+    plt.ylim(-3.5, 3.5)
+    plt.plot(positive_x, positive_y, 'ro')
+    plt.plot(negative_x, negative_y, 'bx')
+    boundary()
+    plt.savefig('result/' + name + '.jpg')
+
+def linear_boundary():
+    plt.plot([-3.5, 3.5], [3.5, -3.5], 'y')
+
+def circular_boundary():
+    circle = Circle((0, 0), radius = 2, color = 'y', fill = False)
+    plt.gca().add_patch(circle)
+
+def boundary_plot():
+    positive_x = [[-2, -2, -1, -1], [-3, 3, 0, 0]]
+    positive_y = [[-2, -1, -2, -1], [0, 0, -3, 3]]
+    negative_x = [[2, 2, 1, 1], [-1, 1, 0, 0]]
+    negative_y = [[2, 1, 2, 1], [0, 0, -1, 1]]
+    boundary = [linear_boundary, circular_boundary]
+    name = ['linear_boundary', 'circular_boundary']
+    for i in xrange(len(positive_x)):
+        plot(positive_x[i], positive_y[i], negative_x[i], negative_y[i], boundary[i], name[i])
+
+def main():
+    spam_filter()
+    boundary_plot()
 
 main()
